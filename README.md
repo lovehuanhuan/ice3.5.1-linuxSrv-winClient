9f5980cd3b4093693fb01b376f7e354c1f74571e
lovehuanhuan <81164785@qq.com>
2018-02-24 13:46:38 +0800

    2 commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+windos端引入include lib和bin（目标文件）
+
+
+
 # ice3.5.1-linuxSrv-winClient
+$ g++ -I. -o server demo.cpp server.cpp -lIce -lIceUtil -lpthread
+$ g++ -I. -o client demo.cpp client.cpp -lIce -lIceUtil -lpthread
+$ ./server
+
+$./client
+Hello World!
---
+++ icecli3-5/icecli3-5/icecli3-5.cpp
@@ -0,0 +1,46 @@
+// icecli3-5.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "stdafx.h"
+#include <Ice/Ice.h>
+#include "Printer.h"
+using namespace Demo;
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    int status = 0;
+    Ice::CommunicatorPtr ic;
+    try {
+        ic = Ice::initialize();
+        Ice::ObjectPrx base = ic->stringToProxy(
+                "SimplePrinter:default -h 172.16.124.8 -p 10000");
+        PrinterPrx printer = PrinterPrx::checkedCast(base);
+        if (!printer)
+            throw "Invalid proxy";
+
+#if 1
+		int i = 0;
+		while(i<1000)
+		{
+			i++;
+			char buf[100]={"Hello World "};
+			sprintf(buf,"%s %d",buf,i);
+			printf(buf);
+			printer->printString(buf);
+		}
+#endif
+    } catch (const Ice::Exception & ex) {
+        cerr << ex << endl;
+        status = 1;
+    } catch (const char * msg) {
+        cerr << msg << endl;
+        status = 1;
+    }
+    if (ic)
+        ic->destroy();
+    return 0;
+}
+
